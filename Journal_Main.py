8d183580a14b484b4ecefc7887503532b5b99564
dromomaniac <evanlhendrix@gmail.com>
2017-05-25 14:40:50 -0300

    First working version of journal scanner

--- Journal_Main.py
+++ Journal_Main.py
@@ -1,5 +1,5 @@
 from subprocess import check_output, call, list2cmdline
-import glob, shutil, os, errno
+import glob, shutil, os, errno, time
 
 
 # location = (some piece fo shit python module that converts city input to coordinates)
@@ -23,21 +23,23 @@ scriptPath = os.path.dirname(os.path.realpath(__file__))
 print 'Start program...'
 
 dateVar = '0'
+dateVarPrior = '0'
 
 while True:
     i = 1
-    while i <= 2:
+    while i <= 5:
         #in = raw_input('If you\'re done, enter 0; if not hit any key)
-
+        print 'Starting new scan. You just scanned date ', dateVarPrior
         # This needs to break out of while loop somehow if the user hits esc during the screencapture interface
         # nameToSave = os.path.join(sShotPath,  + i + '.jpg'
         pathi = os.path.join(scriptPath, 'photo' + str(i) + '.jpg')
         print 'scriptPath = {}'.format(scriptPath)
         print 'path{} = {}'.format(i, pathi)
         call( ['screencapture', '-T', '0', '-W', pathi ])
+        time.sleep(1)
 
         i +=1
-    # Get date and assign the input to the variable dateVar
+    # Get date and assign the input to the variable dateVar. I need to put in some text processing to allow me to omit slashes for speed.
     if dateVar != 0:
         dateVar = '"' + str(raw_input('Enter the fucking date (like this: MM/DD/YY) > ')) + '"'
         print "dateVar variable is: ", dateVar
@@ -61,24 +63,24 @@ while True:
     # print 'filesWoPath = {}'.format(filesWoPath)
 
     # Takes out commas, leaves spaces. (Makes each 'x' ..?
-    spacedFiles = str(' '.join('"' + x + '"' for x in filesGlob))
-    #spacedFiles2 =
-    print 'spacedFiles = {}'.format(spacedFiles)
-
-
-    for j in range(0,i):
-        filesString = []
-        filesString.append(filesGlob[j])
-        print 'filesString variable is: {}'.format(filesString)
+    # spacedFiles = '"' + '" "'.join([str(x) for x in filesGlob]) + '"'
+    # #spacedFiles2 =
+    # print 'spacedFiles = {}'.format(spacedFiles)
+    #
+    #
+    # for j in range(0,i):
+    #     filesString = []
+    #     filesString.append(filesGlob[j])
+    #     print 'filesString variable is: {}'.format(filesString)
 
     # In the command line, this formating works: dayone2 -d '05/17/17' -p "/Users/evanhendrix1/Google Drive/programming/python/JournalPhotos/photo1.jpg" "/Users/evanhendrix1/Google Drive/programming/python/JournalPhotos/photo2.jpg" -- new x
-    call( [ 'dayone2', '-d', dateVar, '-p', spacedFiles, '--', 'new', 'x' ])
-    print list2cmdline([ 'dayone2', '-d', dateVar, '-p', spacedFiles, '--', 'new', 'x' ])
+    call( [ 'dayone2', '-d', dateVar, '-p', filesGlob[0], filesGlob[1], filesGlob[2], filesGlob[3], filesGlob[4], '--', 'new', 'x' ])
+    # print list2cmdline([ 'dayone2', '-d', dateVar, '-p', spacedFiles, '--', 'new', 'x' ])
 
     # Delete directory created earlier
     # shutil.rmtree('/Users/evanhendrix1/Google Drive/programming/python/JournalPhotos/TempSShotDir')
-
-    dateVar = 0
+    dateVarPrior = dateVar
+    dateVar = '0'
     if raw_input('Type 0 if you\'re done, or any other key to do another entry ') == 0:
         break # returns control to if statement
     else:
